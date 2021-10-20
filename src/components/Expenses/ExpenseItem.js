f1983e85650e73d5d47e12b2f295f0372d226460
sidshetty <siddharthshetty@live.com>
2021-10-21 00:33:19 +0530

    added form structure and mock handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,23 @@
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import Card from '../Card/Card';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
+	const [ title, setTitle ] = useState(props.title);
+
+	const clickHandler = () => {
+		setTitle('Updated!');
+	};
+
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">₹{props.amount}</div>
 			</div>
+			<button onClick={clickHandler}>Change Title</button>
 		</Card>
 	);
 };
