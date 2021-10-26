b7a2e476f5f2c95936f82c9ea561afc865066618
sidshetty <siddharthshetty@live.com>
2021-10-26 23:44:49 +0530

    added working filters

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './ExpenseItem.css';
-import Card from '../Card/Card';
+import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
@@ -11,14 +11,16 @@ const ExpenseItem = (props) => {
 	};
 
 	return (
-		<Card className="expense-item">
-			<ExpenseDate date={props.date} />
-			<div className="expense-item__description">
-				<h2>{title}</h2>
-				<div className="expense-item__price">₹{props.amount}</div>
-			</div>
-			<button onClick={clickHandler}>Change Title</button>
-		</Card>
+		<li>
+			<Card className="expense-item">
+				<ExpenseDate date={props.date} />
+				<div className="expense-item__description">
+					<h2>{props.title}</h2>
+					<div className="expense-item__price">₹{props.amount}</div>
+				</div>
+				{/* <button onClick={clickHandler}>Change Title</button> */}
+			</Card>
+		</li>
 	);
 };
 
